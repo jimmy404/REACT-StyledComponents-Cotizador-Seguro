3fbd125a496fb34a8b92c3b0c8fcbd4def41600f
Jimmy <bruno_pier@hotmail.com>
2020-01-25 17:30:40 -0300

    Adding Formulario.js & code

---
+++ src/components/Formulario.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <form>
+            <div>
+                <label>
+                    <select>
+
+                    </select>
+                </label>
+            </div>
+        </form>
+    );
+}
+
+export default Formulario;
