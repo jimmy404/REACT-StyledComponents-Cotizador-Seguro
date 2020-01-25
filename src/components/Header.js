17acffad28fae441c1d7ababf1791a32051d4f66
Jimmy <bruno_pier@hotmail.com>
2020-01-25 17:13:14 -0300

    Adding component styled in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
 
 const Header = ({titulo}) => {
     return (
-        <header>
+        <ContenedorHeader>
             <h1>{titulo}</h1>
-        </header>
+        </ContenedorHeader>
     );
 }
 
