a46e7e619278c5e00300a105784aab72bcadee26
Jimmy <bruno_pier@hotmail.com>
2020-01-25 18:36:48 -0300

    Formula to calculate current price according to brand - helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,25 @@
+//Funcion diferencia de años
+
 export function ObtenerDiferenciaYear(year){
     return new Date().getFullYear() - year;
+}
+
+//Funcion Total segun marca
+
+export function calcularMarca(marca){
+    let incremento;
+    switch(marca){
+        case 'europeo':
+            incremento = 1.30;
+            break;
+        case 'americano':
+            incremento = 1.15;
+            break;
+        case 'asiatico':
+            incremento = 1.05;
+            break;
+        default:
+            break;
+    }
+    return incremento;
 }
