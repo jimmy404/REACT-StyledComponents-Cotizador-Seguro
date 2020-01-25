fdbb44fc4991a3ca62051a4149b71687045a444f
Jimmy <bruno_pier@hotmail.com>
2020-01-25 19:47:08 -0300

    Adding more const styles in Resultado.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Resumen from './components/Resumen';
+import Resultado from './components/Resultado';
 
 import styled from '@emotion/styled';
 
@@ -28,7 +29,7 @@ function App() {
     }
   });
 
-  const { datos } = resumen;
+  const { cotizacion , datos } = resumen;
 
   return (
       <Contenedor>
@@ -40,6 +41,9 @@ function App() {
           guardarResumen={guardarResumen}
           />
           <Resumen datos={datos}/>
+          <Resultado
+          cotizacion={cotizacion}
+          />
         </ContenedorFormulario>
       </Contenedor>
   );
