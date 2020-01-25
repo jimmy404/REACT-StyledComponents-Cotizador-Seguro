7b423eb04a6d73496cf759adb7753b9022801115
Jimmy <bruno_pier@hotmail.com>
2020-01-25 17:15:47 -0300

    Adding other styled component for h1 - Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,18 @@ const ContenedorHeader = styled.header`
     color: #FFFFFF;
 `;
 
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
+
 const Header = ({titulo}) => {
     return (
         <ContenedorHeader>
-            <h1>{titulo}</h1>
+            <TextoHeader>{titulo}</TextoHeader>
         </ContenedorHeader>
     );
 }
