0418e30ad523e9027af42a3c202878b28c8697b8
Jimmy <bruno_pier@hotmail.com>
2020-01-25 19:29:49 -0300

    Adding upercase in ContenedorResumen

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,4 +1,14 @@
 import React, { Fragment } from 'react';
+import styled from '@emotion/styled';
+import { primeraMayuscula } from '../helper';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+`;
 
 const Resumen = ({datos}) => {
 
@@ -7,14 +17,14 @@ const Resumen = ({datos}) => {
     if(marca === '' || year === '' || plan === '' ) return null;
 
     return (
-        <Fragment>
+        <ContenedorResumen>
             <h2>Resumen de Cotizacion</h2>
             <ul>
-                <li>Marca: </li>
-                <li>Plan: </li>
-                <li>Año del auto: </li>
+                <li>Marca: {primeraMayuscula(marca)}</li>
+                <li>Plan: {primeraMayuscula(plan)}</li>
+                <li>Año del auto: {year}</li>
             </ul>
-        </Fragment>
+        </ContenedorResumen>
     );
 }
 
